8fbc6d0201c475a33bfc4d781a00ce757cd00eab
Fattaah0jr <gijilwedus22@gmail.com>
2024-01-17 08:03:41 +0700

    first commit

---
+++ router/joobsheet.js
@@ -0,0 +1,9 @@
+const jobsheetController = require('../controllers/joobsheet');
+const router = require('express').Router();
+
+router.post('/one', jobsheetController.submitOne);
+router.post('/many', jobsheetController.submitMany);
+
+module.exports = router;
+
+//finish 
